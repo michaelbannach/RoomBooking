07b55a862a23691023ef86990bd3777a56effcff
michaelbannach <michaelbannach@gmx.de>
2025-11-22 21:15:44 +0100

    Added the IRoomService interface with CRUD method signatures

--- RoomBooking.Application/Interfaces/IRoomService.cs
+++ RoomBooking.Application/Interfaces/IRoomService.cs
@@ -1,6 +1,16 @@
+using RoomBooking.Domain.Models;
+
 namespace RoomBooking.Application.Interfaces;
 
-public class IRoomService
+public interface IRoomService
 {
-    
+    Task<List<Room>> GetAllRoomsAsync();
+
+    Task<Room?> GetRoomByIdAsync(int roomId);
+
+    Task<(bool added, string? error)> AddRoomAsync(Room room);
+
+    Task<(bool updated, string? error)> UpdateRoomAsync(Room room);
+
+    Task<(bool deleted, string? error)> DeleteRoomByIdAsync(int roomId);
 }
